85a728f14fd6b680b485b26f909868779e6b4f2d
manmyung <manmyung@gmail.com>
2015-04-26 00:08:25 +0900

    Complete tutorial

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [hiccup "1.0.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.170"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler my-webapp.handler/app}
   :profiles
